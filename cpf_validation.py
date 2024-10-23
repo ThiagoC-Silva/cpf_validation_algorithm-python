f0c5f48e83dc48d149b71cb2066ed1363e70caf6
ThiagoC-Silva <thi89248@gmail.com>
2024-10-23 20:51:45 -0300

    Calculation of the first check digit

--- cpf_validation.py
+++ cpf_validation.py
@@ -1,3 +1,22 @@
+def get_check_digit(remainder):
+    if remainder < 2:
+        return 0
+    
+    return 11 - remainder
+
+def calculate_digits():
+    multipliers = list(range(10,1,-1))
+    result = 0
+
+    for index in range(9):
+        product = int(cpf[index]) * multipliers[index]
+        result += product    
+
+    _, remainder = divmod(result, 11)
+
+    digit = get_check_digit(remainder)
+    return str(digit)
+
 def validate_entry():
     while True:
         cpf = input('CPF: ').replace(".", "").replace('-', '')#Remove dots and dashes
@@ -14,3 +33,6 @@ def validate_entry():
         return cpf
 
 cpf = validate_entry()
+first_check_digit = calculate_digits()
+
+print('First check digit:',first_check_digit)
